d942d62a52e82b9806925a27ad17e049759d585e
Oshalusi John <oshalusijohn@gmail.com>
2020-05-11 23:33:00 +0100

    Spam Classification

--- Spam Classification.py
+++ Spam Classification.py
@@ -1,17 +1,27 @@
 # Question Link
 # https://www.codechef.com/problems/SPAMCLAS
+# My Solution, but could not handle large cases
 
-neural_net_num = 2
-minX = 1
-maxX = 4
+cases = int(input())
+for _ in range(cases):
+    neural_net_num, minX, maxX = map(int, input().split())
+    even = 0
+    odd = 0
+    num_array = []
 
-even = 0
-odd = 0
-for n in range(neural_net_num):
-    wi, bi = map(int, input().split())
-    result_arr = []
-    for i in range(minX, maxX+1):
-        x = wi*i + bi
-        result_arr.append(i)
-        print(x)
-print(even, odd)
+    for i in range(minX, maxX + 1):
+        num_array.append(i)
+
+    for i in range(neural_net_num):
+        w, b = map(int, input().split())
+        for k in range(len(num_array)):
+            num = (w * num_array[k]) + b
+            if i == neural_net_num-1:
+                if num % 2 == 0:
+                    even += 1
+                else:
+                    odd += 1
+            else:
+                num_array[k] = num
+    print(even, odd)
+
